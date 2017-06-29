e3b182802be6c73c387f649f39138edca6f460d4
weixuan <weixuan75@163.com>
2017-06-29 09:19:20 +0800

    修改数据库

--- config/db.php
+++ config/db.php
@@ -2,8 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yicangyoupin',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
+    'tablePrefix' => 'ec_',
 ];
