7498f1a3a011dd276ffe4850e841ea2c2270b405
weixuan <weixuan75@163.com>
2017-06-30 11:35:39 +0800

    添加电视节目里的广告内容

---
+++ erp/models/TvlistingsData.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\erp\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%tvlistings_data}}".
+ *
+ * @property integer $id
+ * @property string $sort
+ * @property string $tv_id
+ * @property string $name
+ * @property integer $type
+ * @property integer $pay_time
+ * @property integer $state
+ * @property string $content
+ * @property string $user_id
+ * @property string $create_time
+ */
+class TvlistingsData extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%tvlistings_data}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['sort', 'tv_id', 'name', 'type', 'pay_time', 'content'], 'required'],
+            [['sort', 'tv_id', 'type', 'pay_time', 'state', 'user_id', 'create_time'], 'integer'],
+            [['name'], 'string', 'max' => 100],
+            [['content'], 'string', 'max' => 500],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'sort' => '排序',
+            'tv_id' => '父级ID',
+            'name' => '名称',
+            'type' => '类型：1图片，2视频',
+            'pay_time' => '播放时间（秒）',
+            'state' => '状态',
+            'content' => '介绍',
+            'user_id' => '操作员',
+            'create_time' => '创建时间',
+        ];
+    }
+}
